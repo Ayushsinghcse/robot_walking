864a21147b6baf9348b359c8c664ab1a1f9c4e05
Ayushsinghcse <ayushsingh.rv123@gmail.com>
2025-06-06 05:47:04 +0530

    sixth: many changes many other thing

--- application/main.py
+++ application/main.py
@@ -10,13 +10,10 @@ app = FastAPI(title="Autonomous Wall-Finishing Robot API")
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
-# Include routers
 app.include_router(trajectories.router, prefix="/api/trajectories")
 
-# Mount static files
 app.mount("/static", StaticFiles(directory="application/static"), name="static")
 
-# Serve index.html at root
 @app.get("/")
 async def read_index():
     return FileResponse(Path("application/static/index.html"))
